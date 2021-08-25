54cb09f349db433d6b51ec7ce2998a55181362c2
arcodev <arcodev07@gmail.com>
2021-08-25 14:30:51 -0500

    Terminando navbar y header  en movil

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-alert('hola mundo');
+window.addEventListener('DOMContentLoaded', function() {
+    navMovil();
+});
+
+function navMovil() {
+    const menu = document.querySelector('.menu');
+    if(menu) {
+        menu.addEventListener('click', function() {
+            alert("Anima menur");
+        });
+    }
+}
